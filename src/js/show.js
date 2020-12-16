c4e08eb1336f16b287de6866fad3eccc5780de9d
veronicavc <veronicavelazquezcalleja@gmail.com>
2020-12-16 16:16:37 +0100

    style of details

---
+++ src/js/show.js
@@ -0,0 +1,11 @@
+
+const $pinImg = document.querySelector('.grid-gallery__item')
+$pinImg.addEventListener('click', (event) => {
+    const $clickedPin = event.currentTarget
+    openModal($clickedPin)
+})
+
+function openModal(img) {
+    
+    
+}
